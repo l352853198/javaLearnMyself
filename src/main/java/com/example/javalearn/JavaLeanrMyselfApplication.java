36e27a22fd72d189921c9dec176b26cb79b94598
l352853198 <591753748@qq.com>
2022-09-25 16:38:15 +0800

    构建项目

---
+++ src/main/java/com/example/javalearn/JavaLeanrMyselfApplication.java
@@ -0,0 +1,13 @@
+package com.example.javalearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLeanrMyselfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaLeanrMyselfApplication.class, args);
+    }
+
+}
